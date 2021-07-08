2e25edde89d3ff1cf1d481a8426ceb229b871c4e
UserUNP <39243708+UserUNP@users.noreply.github.com>
2021-07-08 23:29:26 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import time
 import pyautogui
+from pynput.mouse import Controller
 
-def play():
+def play(proc):
     pyautogui.FAILSAFE = False
     with open("data", "r") as file:
         lines = file.readlines()
@@ -11,10 +12,19 @@ def play():
             i += 1
             if i%2 != 0:
                 print(f"WAITING FOR {str(int(float(l)))} SECONDS")
-                time.sleep(float(line))
-            elif i%2 == 0 and len(line.split(" ")) == 2:
+                time.sleep(int(float(line)))
+            elif i%2 == 0 and len(line.split(" ")) == 3 or 4:
                 x = int(line.split(" ")[0])
                 y = int(line.split(" ")[1])
-                print(f"CLICKED AT {x}, {y}")
-                pyautogui.move(x, y)
-                pyautogui.click(x, y)
+                if "RIGHT" in line.split(" "):
+                    print(f"RIGHT CLICKED AT {x}, {y}")
+                    pyautogui.click(x, y, button='right')
+                if "LEFT" in line.split(" "):
+                    print(f"LEFT CLICKED AT {x}, {y}")
+                    pyautogui.click(x, y, button='left')
+                if "SCROLL" in line.split(" "):
+                    dx = int(line.split(" ")[2])
+                    dy = int(line.split(" ")[3])
+                    print(f"SCROLLED {'down' if dy < 0 else 'up'} FROM {x}, {y}")
+                    pyautogui.moveTo(x, y)
+                    Controller().scroll(dx, dy)
